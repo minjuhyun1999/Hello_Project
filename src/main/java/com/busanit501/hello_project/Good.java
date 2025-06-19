4b0e0514b7dbe03bb6625690a57552c378fe2619
minjuhyun1999 <minjoohyun67@naver.com>
2025-06-19 17:08:08 +0900

    혼자 짬뽕 html 만들어 보기

--- src/main/java/com/busanit501/hello_project/Good.java
+++ src/main/java/com/busanit501/hello_project/Good.java
@@ -1,5 +1,39 @@
 package com.busanit501.hello_project;
 
-public class Good {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "juhyun", urlPatterns = "/juhyun")
+
+public class Good extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet !!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet Good 굿 굿굿 베리굿</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+
+
+    }
 }
+
